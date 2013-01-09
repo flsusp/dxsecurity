010f25391a5bae08d1dc18586338ffa83e476853
flsusp <flsusp@gmail.com>
2013-01-09 21:56:19 -0200

    Tests

---
+++ src/test/java/br/com/dextra/security/FakeAuthenticationFailedException1.java
@@ -0,0 +1,8 @@
+package br.com.dextra.security;
+
+import br.com.dextra.security.exceptions.AuthenticationFailedException;
+
+public class FakeAuthenticationFailedException1 extends AuthenticationFailedException {
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/test/java/br/com/dextra/security/FakeAuthenticationFailedException3.java
@@ -0,0 +1,6 @@
+package br.com.dextra.security;
+
+public class FakeAuthenticationFailedException3 extends FakeAuthenticationFailedException2 {
+
+    private static final long serialVersionUID = 1L;
+}
