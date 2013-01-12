8e3ed83fe879e4a3c51db57deebc3042dd75d1a4
flsusp <flsusp@gmail.com>
2013-01-11 22:58:25 -0200

    Refactoring

--- src/main/java/br/com/dextra/security/utils/StringConcatUtil.java
+++ src/main/java/br/com/dextra/security/utils/StringConcatUtil.java
@@ -1,14 +1,10 @@
 package br.com.dextra.security.utils;
 
+import com.google.common.base.Joiner;
+
 public class StringConcatUtil {
 
-	public static String concat(Object... values) {
-		StringBuilder result = new StringBuilder();
-
-		for (Object value : values) {
-			result.append(value);
-		}
-
-		return result.toString();
-	}
+    public static String concat(Object... values) {
+        return Joiner.on("").join(values);
+    }
 }
